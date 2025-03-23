7b579cd31920e14cbb46fe949d35b44fc57fad0e
Fabricio Nathan <fabricionathan749@gmail.com>
2025-03-23 14:16:54 -0300

    projeto pronto

--- src/br/com/projeto/pessoa/main/Main.java
+++ src/br/com/projeto/pessoa/main/Main.java
@@ -1,4 +1,27 @@
 package br.com.projeto.pessoa.main;
 
+import br.com.projeto.pessoa.model.Pessoa;
+
 public class Main {
-}
+    public static void main(String[]args) {
+        Pessoa pessoa = new Pessoa("Nathan", "Policial", 35, 1821821828);
+        Pessoa pessoa2=new Pessoa("Erlon","Eng softw",32,1234567890);
+        Pessoa pessoa3=new Pessoa("Eren","Solador",25,292992929);
+        Pessoa pessoa4=new Pessoa("Ichigo","Espirito",100,292919191);
+        pessoa.calcularHoras(32);
+        pessoa2.calcularHoras(25);
+        pessoa3.calcularHoras(37);
+        pessoa4.calcularHoras(100);
+        System.out.println();
+        System.out.println();
+        System.out.println("APRESENTACAO");
+        System.out.println();
+        pessoa.apresentar();
+        System.out.println();
+        pessoa2.apresentar();
+        System.out.println();
+        pessoa3.apresentar();
+        System.out.println();
+        pessoa4.apresentar();
+
+    }}
--- src/br/com/projeto/pessoa/model/Pessoa.java
+++ src/br/com/projeto/pessoa/model/Pessoa.java
@@ -1,4 +1,24 @@
 package br.com.projeto.pessoa.model;
 
 public class Pessoa {
-}
+    public String nome;
+    public String profissao;
+    public int horasTrab;
+    public int cpf;
+    public double salario;
+
+    public Pessoa(String nome,String profissao, int horasTrab,int cpf ){
+        this.nome=nome;
+        this.profissao=profissao;
+        this.cpf=cpf;
+        this.horasTrab=horasTrab;
+    }
+    public void calcularHoras(double valorhor){
+        this.salario=this.horasTrab*valorhor;
+        System.out.printf("O salario é %f",this.salario);
+    }
+    public void apresentar(){
+        System.out.printf("Meu nome é %s, meu cpf é %d, minha profissao é %s , e a quantidade de horas que trabalho é %d, e meu salario %.2f",this.nome,this.cpf,this.profissao,this.horasTrab,this.salario);
+    }
+
+    }
